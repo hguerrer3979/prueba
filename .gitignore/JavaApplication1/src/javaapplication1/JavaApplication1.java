633194b70017a1ceecc577a491920c122df7ff0c
hguerrer3979 <32824313+hguerrer3979@users.noreply.github.com>
2018-03-04 23:46:17 -0500

    Add files via upload

---
+++ .gitignore/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/**
+ * Bucle while que termina cuando se introduce por teclado un
+ * número negativo.
+ *
+ * @author Luis José Sánchez
+ */
+
+/**
+*  Generación de números aleatorios.
+*
+*  @author Luis José Sánchez
+**/
+
+public class JavaApplication1 {
+  public static void main(String[] args) {
+
+    System.out.println("Diez números aleatorios:\n");
+
+    for (int i = 1; i < 11; i++) {
+      System.out.println(Math.random());
+    }
+  }
+}
